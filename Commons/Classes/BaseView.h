e1cab1a87fa4ec0862ee0f5ad35bfe043d985ed4
yr <1085309750@qq.com>
2017-12-03 15:49:33 +0800

    uodate

---
+++ Commons/Classes/BaseView.h
@@ -0,0 +1,35 @@
+//
+//  BaseView.h
+//  SEUDApp
+//
+//  Created by Souyun on 16/6/1.
+//  Copyright © 2016年 chuangwuxian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BaseView : UIView
+
+-(void)addSubView:(UIView*)view;
+-(void)removeFromSuperview;
+
+-(void)postEvent:(NSString*)name;
+-(void)postEvent:(NSString*)name data:(id)data;
+@property(nonatomic,weak)id parent;
+@property(nonatomic,readonly)id model;
+
+
+///////////////////////////////// DZComponent /////////////////////////////////
+
+-(BOOL)diff:(NSString*)key;
+-(void)render;
+-(NSDictionary*)schema;
+-(NSDictionary*)readSchema:(NSString*)name;
+-(void)clearDiff;
+-(void)startRandomState;
+-(void)stopRandomState;
+
+@property(nonatomic,strong)NSDictionary* state;
+
+
+@end
